7bc98d7dc0f745749078470a5a500c02b09c0759
KawasakiHachi <kd1221092@st.kobedenshi.ac.jp>
2018-11-14 09:43:19 +0900

    kwsk first commit

---
+++ project.php
@@ -0,0 +1,18 @@
+<?php 
+require_once __DIR__ . '/header.php';
+ ?>
+ タグ：<input type="text" value="プログラマー">　　募集スキル：<input type="text">　　プロジェクト詳細：<input type="text"><form action="project.php"><input type="submit" value="検索"></form><div class="box4">
+    <a href="gamecreate.php"><h3>ゲーム制作メンバー募集！</h3></a>
+    <p>現在ゲーム制作の為プログラム、グラフィック、サウンド担当者を募集しています！</p>
+    <p>登録タグ：<b>プログラマー</b>、グラフィックデザイナー、サウンドクリエイター、ゲーム制作</p>
+</div>
+<div class="box4">
+    <a href=""><h3>目指せドラえもん？！</h3></a>
+    <p>アニメでよく見るドラえもんの道具を実際に作ってみませんか？</p>
+    <p>登録タグ：ハードウェア、ソフトウェア、<b>プログラマー</b>、デザイナー</p>
+</div>
+<div class="box4">
+    <a href=""><h3>アニメ「サマーウォーズ」のoz実現を目指して</h3></a>
+    <p>サマーウォーズのozのように、誰でも、どんな国の人でも楽しく会話できる世界を作りたいです。</p>
+    <p>登録タグ：<b>プログラマー</b>、デザイナー、キャラクター、フィールド、翻訳</p>
+</div>
