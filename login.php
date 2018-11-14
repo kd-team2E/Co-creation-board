8943f8ad22121275792f8e482e68dc8f4a20743d
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2018-11-14 12:08:58 +0900

    ADD 0J02011 担当箇所

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>プロジェクト掲示板</title>
+	<link rel="stylesheet" type="text/css" href="project.css">
+</head>
+<body>
+<div align="right"><a href="signup.php">新規登録</a> | <a href="login.php">ログイン</a></div>
+<h2>プロジェクト掲示板</h2>
+<hr>
+
+mail : <input type="email" value="example.co.jp"><br>
+pass : <input type="password" value="BeOS"><br><br>
+
+<input type="button" onclick="location.href='project.php'" value="ログイン">
+
+</body>
+</html>
---
+++ signup_success.php
@@ -0,0 +1,14 @@
+<?php
+    require_once __DIR__ . '/header.php';
+?>
+
+<DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="project.css">
+</head>
+<body>
+    <p>登録が完了しました。
+</body>
+</html>
