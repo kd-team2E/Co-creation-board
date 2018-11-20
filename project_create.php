514579e21b5481b39c08c86529db086c3f05b878
takayamaroso <kd1242223@st.kobedenshi.ac.jp>
2018-11-20 11:15:21 +0900

    require_onceのPATHの書き方修正'

--- project_create.php
+++ project_create.php
@@ -1,5 +1,5 @@
 <?php
-    require_once __DIR__ . './header.php';
+    require_once __DIR__ . '/header.php';
 
     session_start();
     $projectName = isset($_SESSION['project']) ? $_SESSION['project'] : '';
