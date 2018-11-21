af3f77eed784f98015acd6fa58742c0356e8af39
KawasakiHachi <kd1221092@st.kobedenshi.ac.jp>
2018-11-21 11:10:03 +0900

    ヘッダーファイル名修正

--- header2.php
+++ header2.php
@@ -5,7 +5,7 @@
 	<link rel="stylesheet" type="text/css" href="project.css">
 </head>
 <body>
-<div align="right"><a href="user_detail.php">マイページ</a>/ログアウト</div>
+<div align="right"><a href="signup.php">新規登録</a> | <a href="login.php">ログイン</a></div>
 <h2>プロジェクト掲示板</h2>
 <ul class="navi">
 <li><a href="project_search.php">プロジェクト検索</a></li><li>|</li>
